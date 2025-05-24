363f149a9a964827d67e59b8518caab77a425a38
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-24 17:55:55 +0500

    imp

--- lib/pages/play_page.dart
+++ lib/pages/play_page.dart
@@ -1,13 +1,44 @@
+import 'package:billie_eilish_d5/main.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class PlayPage extends StatefulWidget {
-  const PlayPage({super.key});
+  final MusicModel music;
+
+  const PlayPage({super.key, required this.music});
 
   @override
   State<PlayPage> createState() => _PlayPageState();
 }
 
 class _PlayPageState extends State<PlayPage> {
+  AudioPlayer player = AudioPlayer();
+  var duration = Duration.zero;
+
+  @override
+  void initState() {
+    setUp();
+    super.initState();
+  }
+
+  void setUp() async {
+    await player.setAsset(widget.music.asset);
+    duration = player.duration ?? duration;
+    setState(() {});
+  }
+
+  String format(Duration dur) {
+    final min = dur.inMinutes;
+    final sec = dur.inSeconds % 60;
+    return "$min:${sec.toString().padLeft(2, "0")}";
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
