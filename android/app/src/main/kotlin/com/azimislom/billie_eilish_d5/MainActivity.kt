2d735962dcab6ea1ffe4c8e8be71ed0ea46169b7
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-24 17:24:16 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/azimislom/billie_eilish_d5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.azimislom.billie_eilish_d5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/play_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlayPage extends StatefulWidget {
+  const PlayPage({super.key});
+
+  @override
+  State<PlayPage> createState() => _PlayPageState();
+}
+
+class _PlayPageState extends State<PlayPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+        ),
+      ),
+    );
+  }
+}
